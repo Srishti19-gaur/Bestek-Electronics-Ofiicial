314327afd1cac65761d3b9848264847ae05f6db2
Srishti19-gaur <84332258+Srishti19-gaur@users.noreply.github.com>
2024-07-05 12:10:03 +0530

    updated the order buttons

---
+++ assets/js/ordernow.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  }
+
+  var productName = getUrlParameter('product');
+
+  document.getElementById('product-details').innerText = productName;
+});
